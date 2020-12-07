bab3d45a2ee6a84fdf9c3dce87ca9122cc1f7dc8
Roman Peters <mail@romanpeters.nl>
2020-12-07 18:44:36 +0100

    Add /height

--- main.example.py
+++ main.example.py
@@ -1,14 +1,25 @@
-#!/usr/bin/python3.7
+#!/usr/bin/python3.8
 
 from flask import Flask, request
 import subprocess
 
 app = Flask(__name__)
 
+script = "idasen-controller/main.py"
+
+
+@app.route('/', methods=['GET'])
+def height():
+    try:
+        result = subprocess.check_output(f"python3 {script} | grep Height:", shell=True)
+        return result
+    except subprocess.CalledProcessError as e:
+        return "An error occurred while trying to change desk status: {}".format(e)
+
 @app.route('/sit', methods=['GET'])
 def sit():
     try:
-        result = subprocess.check_output("python3 /home/username/idasen-control/idasen-controller/main.py --sit", shell=True)
+        result = subprocess.check_output(f"python3 {script} --sit", shell=True)
         return result
     except subprocess.CalledProcessError as e:
         return "An error occurred while trying to change desk status: {}".format(e)
@@ -16,7 +27,7 @@ def sit():
 @app.route('/stand', methods=['GET'])
 def stand():
     try:
-        result = subprocess.check_output("python3 /home/username/idasen-control/idasen-controller/main.py --stand", shell=True)
+        result = subprocess.check_output(f"python3 {script} --stand", shell=True)
         return result
     except subprocess.CalledProcessError as e:
         return "An error occurred while trying to change desk status: {}".format(e)
